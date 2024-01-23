3e2c206fb460acb623aa79a0c0410ddfe9e23859
Pengu1Nus <mustafin.zulfat@yandex.ru>
2024-01-23 21:04:29 +0300

    feat: add solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import { join } from 'path';
+import { writeFile, access } from 'fs/promises';
+import { getPath } from '../utils/pathSolver.js';
+
+const { __filename, __dirname } = getPath(import.meta.url);
+const filePath = join(__dirname, 'files', 'fresh.txt');
+const text = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+  const fileExists = await isFileExists(filePath);
+  if (!fileExists) {
+    await writeFile(filePath, text);
+  } else {
+    throw new Error('FS operation failed');
+  }
 };
+
+const isFileExists = async(file) => {
+  try {
+    await access(file)
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
 
 await create();
