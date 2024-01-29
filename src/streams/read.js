6ddd9ca50b17ae5198398225a51484b6d9f16d0b
Pengu1Nus <mustafin.zulfat@yandex.ru>
2024-01-30 01:17:20 +0300

    feat: add solutions to stream tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import { getPath } from '../utils/pathSolver.js';
+
+const { __dirname } = getPath(import.meta.url);
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+  const readStream = fs.createReadStream(filePath);
+
+  readStream.on('data', (chunk) => {
+    process.stdout.write(chunk);
+  });
+
+  readStream.on('end', () => {
+    process.stdout.write('\n');
+  });
+
+  readStream.on('error', (err) => {
+    console.error('Error:', err);
+  });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+import { getPath } from '../utils/pathSolver.js';
+
+const { __dirname } = getPath(import.meta.url);
+
 const write = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+  const writeStream = fs.createWriteStream(filePath);
+
+  process.stdin.pipe(writeStream);
+
+  process.stdin.on('end', () => {
+    console.log('Finished writing to file');
+  });
+
+  writeStream.on('error', (err) => {
+    console.error('An error occurred:', err);
+  });
 };
 
 await write();
