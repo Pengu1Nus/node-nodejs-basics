75ccf98e77ac028db5ae9e4d35ae5b79045b94c4
Pengu1Nus <mustafin.zulfat@yandex.ru>
2024-01-30 00:19:38 +0300

    feat: add solution to read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { getPath } from '../utils/pathSolver.js';
+
+const { __dirname } = getPath(import.meta.url);
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+  try {
+    await fs.access(filePath);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
+
+  const content = await fs.readFile(filePath, 'utf8');
+  console.log(content);
 };
 
 await read();
