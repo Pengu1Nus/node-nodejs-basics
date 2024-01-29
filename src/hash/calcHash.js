a20606fa2e12b28d9b5c5fb1661569ee51f65d27
Pengu1Nus <mustafin.zulfat@yandex.ru>
2024-01-30 01:12:03 +0300

    feat: add hash solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import crypto from 'crypto';
+import fs from 'fs';
+import path from 'path';
+import { getPath } from '../utils/pathSolver.js';
+
+const { __dirname } = getPath(import.meta.url);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const hash = crypto.createHash('sha256');
+  const input = fs.createReadStream(filePath);
+
+  input.on('readable', () => {
+    const data = input.read();
+    if (data) hash.update(data);
+    else {
+      console.log(hash.digest('hex'));
+    }
+  });
 };
 
 await calculateHash();
