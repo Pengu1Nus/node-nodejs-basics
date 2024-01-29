4a9023658cf75b08e35838e1373884c7a6f4951a
Pengu1Nus <mustafin.zulfat@yandex.ru>
2024-01-30 00:16:18 +0300

    feat: add solution to list and delete tasks

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { getPath } from '../utils/pathSolver.js';
+
+const { __dirname } = getPath(import.meta.url);
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fileToRemove.txt');
+
+  try {
+    await fs.access(filePath);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
+  await fs.unlink(filePath);
 };
 
 await remove();
